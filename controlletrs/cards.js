f78d3c0dd18a2c2d36ae343dc7985886e2e85eb5
Dmitriy Bezrukov <dima1989o-z@yandex.ru>
2023-09-08 03:29:44 +0300

    Фикс автотеста

--- controlletrs/cards.js
+++ controlletrs/cards.js
@@ -78,7 +78,7 @@ module.exports.dislikeCard = (req, res) => {
         res.send(card);
       })
       .catch((err) => {
-        res.status(404).send({ message: 'Карточка с указанным _id не найдена', err });
+        res.status(400).send({ message: 'Карточка с указанным _id не найдена', err });
       });
   } else {
     res.status(400).send({ message: 'Некорректный _id карточки' });
