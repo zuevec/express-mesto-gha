a4f44cf4a71b3e5482bcf4da867f690b09a762be
Dmitriy Bezrukov <dima1989o-z@yandex.ru>
2023-09-08 03:23:36 +0300

    Фикс автотеста user с несуществующим id

--- controlletrs/users.js
+++ controlletrs/users.js
@@ -23,10 +23,18 @@ module.exports.getUser = (req, res) => {
   if (req.params.userId.length === 24) {
     User.findById(req.params.userId)
       .then((user) => {
+        if (!user) {
+          res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+          return;
+        }
         res.status(200).send(user);
       })
       .catch((err) => {
-        res.status(404).send({ message: 'Пользователь по указанному _id не найден.', err });
+        if (err.statusCode === 404) {
+          res.status(404).send({ message: 'Пользователь по указанному _id не найден.', err });
+        } else {
+          res.status(500).send({ message: 'На сервере произошла ошибка', err });
+        }
       });
   } else {
     res.status(400).send({ message: 'Некорректный _id пользователя' });
