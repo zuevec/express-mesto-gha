eda0d07333534393587e2cc37c109ca001138c7b
Dmitriy Bezrukov <dima1989o-z@yandex.ru>
2023-09-18 02:06:09 +0300

    Тест автотестов

--- routes/cards.js
+++ routes/cards.js
@@ -23,13 +23,13 @@ router.post('/', celebrate({
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required(),
+    cardId: Joi.string().length(24).alphanum().required(),
   }).unknown(true),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required(),
+    cardId: Joi.string().length(24).alphanum().required(),
   }).unknown(true),
 }), dislikeCard);
 
