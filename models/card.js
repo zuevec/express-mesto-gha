d552e50730a7f20ece807a18484ef0ec934aa3e6
Dmitriy Bezrukov <dima1989o-z@yandex.ru>
2023-09-18 05:00:39 +0300

    Автотесты

--- models/card.js
+++ models/card.js
@@ -23,11 +23,11 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  likes: {
+  likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    default: [],
-  },
+    default: Array,
+  }],
   createdAt: {
     type: Date,
     default: Date.now,
